330120f46e5fcb58f7193043d364bc6948084e9d
Heather Welche <heather.welch@noaa.gov>
2018-02-04 13:45:12 -0800

    more songs

--- beatr/R/beatr_function.R
+++ beatr/R/beatr_function.R
@@ -12,7 +12,11 @@
 
 beatr=function(sound){
   soundwaves=c(
-    tubular="tubular.wav"
+    tubular="shear_tubular.wav",
+    beak="shear_beak.wav",
+    essential="shear_essential.wav",
+    swift="shear_swift.wav",
+    django="shear_django.wav"
   )
   sound_path <- file.path(find.package("beatr"), "adlibs", soundwaves[sound])
   beepr::beep(sound_path)
