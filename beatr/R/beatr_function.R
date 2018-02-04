33bc3f574591e77902f5018e2f048f3f805f4063
Heather Welche <heather.welch@noaa.gov>
2018-02-03 17:07:54 -0800

    more nasty edits

--- beatr/R/beatr_function.R
+++ beatr/R/beatr_function.R
@@ -1,4 +1,4 @@
-#' A Cat Function
+#' A nastyverse function
 #'
 #' This function allows you to express your love of cats.
 #' @param love Do you love cats? Defaults to TRUE.
@@ -6,20 +6,12 @@
 #' @export
 #' @examples # Play a nasty sound beatr("tubular")
 #' @import beepr
-#' cat_function()
+
 
 
 
 beatr=function(sound){
-  beep(paste0("R/sysdata.rda/",sound,".wav"))
+  beepr::beep(paste0("R/sysdata.rda/",sound,".wav"))
 }
 
 
-cat_function <- function(love=TRUE){
-  if(love==TRUE){
-    print("I love cats!")
-  }
-  else {
-    print("I am not a cool person.")
-  }
-}
